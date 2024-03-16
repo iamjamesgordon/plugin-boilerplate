994047bf727c3e7ea73df777c78b2a315b4378c4
iamjamesgordon <james@neontribe.co.uk>
2024-03-16 13:53:42 +0000

    Adding a blank plugin
    A blank plugin has been added with all necessary functions to get loaded. This can act as a start point with the intention to be updated later on

---
+++ plugins/plugin-boilerplate/includes/class-plugin-boilerplate-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/iamjamesgordon/plugin-boilerplate
+ * @since      1.0.0
+ *
+ * @package    Plugin_Boilerplate
+ * @subpackage Plugin_Boilerplate/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Plugin_Boilerplate
+ * @subpackage Plugin_Boilerplate/includes
+ * @author     James Gordon <iamjamesgordon@gmail.com>
+ */
+class Plugin_Boilerplate_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
